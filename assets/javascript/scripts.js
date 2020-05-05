6724119054ea2cd663e4b93811f84407ff3fe02f
Douglas Walas Rodrigues Pardim <douglas.skd@gmail.com>
2020-05-05 17:54:39 +0000

    adding more functions and make the menu bar

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -24,18 +24,27 @@ function loadCards(data,filtro=0,inicio=0){
 
 }
 
+
 async function loadPage(){
     let data = await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72");
     loadCards(await data.json(),filter,index);
-    let pages = document.querySelectorAll("body > .container > .filtro");
+    let pages = document.querySelectorAll("body > .container > .buttons > .filtro");
     pages[0].children[atual_page].classList.add('atual');
+
+    document.querySelector("body > .container > .buttons > .comodations").innerHTML = "<p>1 - 8 de 24 acomodações</p>";
+     
 }
 
 async function nextPage(offset,button){
     atual_page = button;
     let data = await fetch("https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72");
     loadCards(await data.json(),filter+offset,index+offset);
-    let pages = document.querySelectorAll("body > .container > .filtro");
+    let pages = document.querySelectorAll("body > .container > .buttons > .filtro");
+    acomodation = atual_page===0?"1 - 8":atual_page===1?"9 - 16":"17 - 24";
+    frase = `${acomodation} de 24 acomodações`;
+
+    document.querySelector("body > .container > .buttons > .comodations").innerHTML = "<p>" + frase +"</p>";
+
     let listData = Array.prototype.slice.call(pages[0].children, 0);
     listData.map(item=>item.classList.remove('atual'));
     pages[0].children[atual_page].classList.add('atual');
